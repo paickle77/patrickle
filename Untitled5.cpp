ad7bdf20c0851f9e00e3bd7b45531e4ac82a32b7
paickle77 <141712547+paickle77@users.noreply.github.com>
2023-08-09 00:34:45 +0700

    tìm số chẵn số lẻ

---
+++ Untitled5.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main (){
+	int a;
+	printf("nhap so a:\n");
+	scanf("%d",&a);
+	if (a%2==0){
+		printf("%d la so chan\n",a);
+	}else 
+	printf("%d la so le",a);
+	return 0;
+}
