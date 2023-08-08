21cb3632086a10eb7cd111663676898e8704ddfe
paickle77 <141712547+paickle77@users.noreply.github.com>
2023-08-08 16:05:47 +0700

    bai tap1

---
+++ Untitled2.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+	int a,b;
+	printf("nhap gia tri cua a\n");
+	scanf("%d",&a);
+	printf("nhap gia tri cua b\n");
+	scanf("%d",&b);
+	int c;
+	c=a+b;
+	printf("c la tong a va b:%d",c);
+	return 0;
+}
